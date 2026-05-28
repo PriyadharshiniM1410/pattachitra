e94f1d408db1c5785e106486d6e8b1408dfd5090
PriyadharshiniM1410 <m.priyadharshini141006@gmail.com>
2026-05-28 15:37:33 +0530

    add single art detail page

--- components/GalleryClient.tsx
+++ components/GalleryClient.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
 import { urlFor } from '../sanity/lib/image'
 
 export default function GalleryClient({
@@ -59,41 +60,36 @@ export default function GalleryClient({
       {/* GRID */}
       <div className="gallery-grid">
         {filteredArtworks.map((art: any) => (
-          <div
-            className="gallery-card"
+          <Link
+            href={`/art/${art.slug?.current}`}
             key={art._id}
+            style={{ textDecoration: 'none', color: 'inherit' }}
           >
-            {art.image && (
-              <div className="gallery-image-wrapper">
-                <img
-                  src={urlFor(art.image)
-                    .width(800)
-                    .url()}
-                  alt={art.name}
-                  className="gallery-img"
-                />
-              </div>
-            )}
-
-            <div className="gallery-content">
-              {art.category && (
-                <span className="gallery-tag">
-                  {art.category}
-                </span>
+            <div className="gallery-card">
+              {art.image && (
+                <div className="gallery-image-wrapper">
+                  <img
+                    src={urlFor(art.image).width(800).url()}
+                    alt={art.name}
+                    className="gallery-img"
+                  />
+                </div>
               )}
-
-              <h3>{art.name}</h3>
-
-             {art.description && (
-  <p>
-    {art.description.length > 500
-      ? art.description.slice(0, 500) + '...'
-      : art.description}
-  </p>
-)}
-            
+              <div className="gallery-content">
+                {art.category && (
+                  <span className="gallery-tag">{art.category}</span>
+                )}
+                <h3>{art.name}</h3>
+                {art.description && (
+                  <p>
+                    {art.description.length > 500
+                      ? art.description.slice(0, 500) + '...'
+                      : art.description}
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </main>
