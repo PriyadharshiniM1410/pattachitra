c61bc889ace653df216a783e42d2967d0a65e45e
PriyadharshiniM1410 <m.priyadharshini141006@gmail.com>
2026-06-25 14:40:22 +0530

    feat: add api token

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,11 @@
-export const apiVersion = '2026-04-14'
+export const projectId =
+  process.env.SANITY_PROJECT_ID || 'ydfrecr6'
 
-export const dataset = 'production'
+export const dataset =
+  process.env.SANITY_DATASET || 'production'
 
-export const projectId = 'ydfrecr6'
+export const apiVersion =
+  process.env.SANITY_API_VERSION || '2026-04-14'
+
+export const token =
+  process.env.SANITY_API_TOKEN || 'sk8dQztVU3r3FCsfZqaHWb9ByqLHtxKxtDUUgZ0hEyPCS0qMwWiGtrtsu0fHjSaH2MdPWLYiqScehi5DeE1Fw0oR1YVeP7AJED0QyR5EDkki4AfirzU54VCchR6HbTz57a6eRt0DUurXKVx3yD0lkRROEymOA9GbWVDDrkEImOlHUPzHaEU8'
